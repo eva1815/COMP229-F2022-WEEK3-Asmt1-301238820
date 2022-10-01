60b7e1bde2ecbc313225b95a72ea83f90319068e
eva1815 <evafan1815@gmail.com>
2022-10-01 19:30:58 -0400

    views and partials completed

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,11 +1,20 @@
 // Index Routes 
 import { Router } from 'express';
-import {helloWorld} from '../controllers/index.controller.server.js';
+import {displayHomePage, 
+        displayAboutPage, 
+        displayServicesPage, 
+        displayProjectsPage, 
+        displayContactPage} from '../controllers/index.controller.server.js';
 
 const router = Router();
 
 // add middleware to connect application
-router.get('/',helloWorld);
-router.get('/hello',helloWorld);
+router.get('/',displayHomePage);
+router.get('/home',displayHomePage);
+router.get('/about', displayAboutPage);
+router.get('/projects', displayProjectsPage);
+router.get('/services', displayServicesPage);
+router.get('/contact', displayContactPage);
+
 
 export default router;
