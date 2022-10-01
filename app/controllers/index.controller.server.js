5a5e943baf4e94cef18db7bf12bd9fa387ccd780
eva1815 <evafan1815@gmail.com>
2022-10-01 11:58:15 -0400

    View Engine Created

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,6 +1,5 @@
 
 //Index controller
 export function helloWorld(req, res, next){
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello Hello World from controller');
+    res.render('index');
 };
